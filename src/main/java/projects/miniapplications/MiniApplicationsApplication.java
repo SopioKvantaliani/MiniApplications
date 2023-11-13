ac694f8f2508aebeb58fac97769528816acc5b81
sopiokvantaliani <skvantaliani@gmail.com>
2023-11-13 15:38:31 -0500

    MiniApplications_Projects

---
+++ src/main/java/projects/miniapplications/MiniApplicationsApplication.java
@@ -0,0 +1,13 @@
+package projects.miniapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/miniapplications/MiniApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.miniapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
