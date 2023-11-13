bfabeaaf43192f3f0b6f98eb1d46f35c73514aff
sopiokvantaliani <skvantaliani@gmail.com>
2023-11-13 16:09:01 -0500

    PasswordValidator method. isValidPassword method checks password criteria.

--- src/main/java/projects/miniapplications/MiniApplicationsApplication.java
+++ src/main/java/projects/miniapplications/MiniApplicationsApplication.java
@@ -2,12 +2,17 @@ package projects.miniapplications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import projects.miniapplications.task1.PasswordValidator;
 
 @SpringBootApplication
 public class MiniApplicationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MiniApplicationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MiniApplicationsApplication.class, args);
+
+        PasswordValidator.checkApplicationPassword();
+
+
+    }
 
 }
---
+++ src/main/java/projects/miniapplications/task1/PasswordValidator.java
@@ -0,0 +1,31 @@
+package projects.miniapplications.task1;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+
+    public static void checkApplicationPassword(){
+        Scanner scanner = new Scanner(System.in);
+        boolean isValid = false;
+
+        while (!isValid){
+
+            System.out.println("Please enter Password");
+            String password = scanner.nextLine();
+
+            if (isValidPassword (password)){
+                isValid=true;
+                System.out.println("Password is valid");
+            }else{
+                System.out.println("Invalid password! Password should have no more than 3 characters, and can only contain uppercase/lowercase letters or numbers.");
+            }
+        }
+        scanner.close();
+}
+
+
+
+    private static boolean isValidPassword (String password){
+        return !password.trim().isEmpty() && password.matches("[A-Za-z0-9]{1,3}");
+    }
+}
